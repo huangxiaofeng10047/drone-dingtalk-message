b308976f7bfa7f0bf30b720d01c3d0bd53531799
郭旭东 <guoxudong@keking.cn>
2019-07-17 10:22:06 +0800

    test repo list

--- plugin.go
+++ plugin.go
@@ -268,9 +268,9 @@ func (p *Plugin) markdownTpl() string {
 			content := strings.Split(reponame, ":")[0]
 			RepoName := strings.Split(content, "/")[1]
 			Image := strings.Split(content, "/")[2]
-			repoinfo := fmt.Sprintf("- %s 镜像：%s", Image, reponame)
+			repoinfo := fmt.Sprintf("> %s 镜像地址：%s", Image, reponame)
 			deployUrl := fmt.Sprintf("https://devops.keking.cn/#/k8s/imagetag?namespace=%s&reponame=%s", RepoName, Image)
-			deployLink := fmt.Sprintf(" [部署 %s ](%s)", Image, deployUrl)
+			deployLink := fmt.Sprintf("> [部署 %s ](%s)", Image, deployUrl)
 			tpl += repoinfo + "\n\n" + deployLink + "\n\n"
 		}
 		//repoinfo := fmt.Sprintf("> Docker 镜像：%s", imagepath)
